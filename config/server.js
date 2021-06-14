4403eba4944109e5ee9665fdeacfa100715733cc
Dmitry Kizilov <dkizilov@gmail.com>
2021-06-14 11:32:20 +0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3a5be820f7a08e6433217ad58491c885'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '15f3781b-2dde-485b-b358-048af23b8e11'
+};
